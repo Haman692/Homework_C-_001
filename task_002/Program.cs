9696b7e54a7848aa810cd302efbcc9919416d201
Haman692 <haman692@yandex.ru>
2022-11-22 10:28:19 +0700

    Second task

---
+++ task_002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine ("Напишите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Напишите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine ());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше числа {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число {numberB} больше числа {numberA}");
+}
