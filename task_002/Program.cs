07e7390a70dd5f7e7b9bde75a419120cf7129de6
Haman692 <haman692@yandex.ru>
2022-11-22 14:07:28 +0700

    Промежуточный результат

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
 Console.WriteLine ("Напишите число A ");
 int numberA = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine ("Напишите число B ");
---
+++ task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine ("Напишите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Напишите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Напишите число C ");
+int numberC = Convert.ToInt32(Console.ReadLine ());
+int max = numberA;
+if ((numberB < numberC) && (max < numberC ))
+{
+    max = numberC;
+}
