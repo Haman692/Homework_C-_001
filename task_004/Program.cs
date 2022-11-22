5127945b67052aeb5429efd2dc4370fbcedef11a
Haman692 <Haman692@yandex.ru>
2022-11-22 17:25:05 +0700

    Комит

--- task_004/Program.cs
+++ task_004/Program.cs
@@ -6,8 +6,8 @@ Console.WriteLine ("Напишите число B ");
 int numberB = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine ("Напишите число C ");
 int numberC = Convert.ToInt32(Console.ReadLine ());
-if ((numberB < numberC) && (numberA < numberC)) Console.WriteLine(numberC);
+int max = numberA;
+if ((numberB < numberC) && (numberA < numberC)) max = numberC;
 
-if ((numberB < numberA) && (numberC < numberA)) Console.WriteLine(numberA);
-
-if ((numberA < numberB) && (numberC < numberB)) Console.WriteLine(numberB);
+if ((numberA < numberB) && (numberC < numberB)) max = numberB;
+Console.WriteLine($"Максимальное {max}");
